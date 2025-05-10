787235a8aec258e7a04781fcdb0e81432b70fddd
mat-0 <Github@thechels.uk>
2025-05-10 18:21:12 +0100

    Initial Commit

---
+++ Hyd/HydApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydApp.swift
+//  Hyd
+//
+//  Created by Mat Benfield on 10/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HydApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
