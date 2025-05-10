9c35206690d4d6240c594e41d34f5896f1d41a8e
mat-0 <Github@thechels.uk>
2025-05-10 19:38:37 +0100

    fixed
    Signed-off-by: mat-0 <Github@thechels.uk>

--- Hyd/z_HydApp.swift
+++ Hyd/z_HydApp.swift
@@ -1,11 +1,13 @@
 //
-//  HydApp.swift
+//  z_HydApp.swift
 //  Hyd
 //
 //  Created by Mat Benfield on 10/05/2025.
 //
 
 import SwiftUI
+// Add this import to ensure ContentView is visible
+import ContentView
 
 @main
 struct HydApp: App {
